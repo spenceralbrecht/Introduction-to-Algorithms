9b040c7e41cd8253dc837e5345cced42bfffa74e
Spencer Albrecht <Spencer@college8-32-236.resnet.ucsc.edu>
2017-01-16 14:56:01 -0800

    Working on Lex.java. File IO works but still fixing bugs with sorting

---
+++ pa1/Lex.java
@@ -0,0 +1,74 @@
+import java.io.*;
+import java.util.Scanner;
+class Lex {
+    public static void main(String[] args) throws IOException {
+        if(args.length!=2) {
+            //throw new RuntimeException("Usage: Lex file1 file2");
+            System.err.println("Usage: Lex infile outfile");
+            System.exit(1);
+        }
+        Scanner fileIn = null;
+        PrintWriter fileOut = null;
+        String line = null;
+        String[] fileLines = null;
+        int x, lineNumber = 0;
+
+        fileIn = new Scanner(new File(args[0]));
+        fileOut = new PrintWriter(new FileWriter(args[1]));
+
+        while( fileIn.hasNextLine() ){
+            lineNumber++;
+            fileIn.nextLine();
+            // line = in.nextLine()+" ";    // add extra space so split works right
+            // token = line.split("\\s+");  // split line around white space
+            // n = token.length;
+            // out.println("Line "+lineNumber+" contains "+n+" token"+(n==1?"":"s")+":");
+            // for(i=0; i<n; i++){
+            //     out.println(token[i]);
+            // }
+            // out.println();
+        }
+
+        fileIn.close();
+        fileIn = new Scanner(new File(args[0]));
+
+        fileLines = new String[lineNumber];
+        for (int i = 0; i < lineNumber; i++) {
+            fileLines[i] = fileIn.nextLine();
+            //System.out.println(fileLines[i]);
+        }
+
+        List tempList = new List();
+        tempList.moveFront();
+        for (int i = 0; i < lineNumber; i++) {
+            if (i==0) {
+                tempList.prepend(i);
+            }
+            else {
+                int j = i-1;
+                // If it comes before fileLines[i-1]
+                while(fileLines[i].compareTo(fileLines[j])<0 && j>0) {
+                    j--;
+                    tempList.movePrev();
+                }
+                tempList.insertAfter(i);
+            }
+        }
+
+
+
+
+        // Prints the correct output to the file
+        tempList.moveFront();
+        while(tempList.index()>=0){
+            x = tempList.get();
+            // Prints the correct array element
+            // based on the sorted List
+            fileOut.println(fileLines[x]);
+            tempList.moveNext();
+        }
+
+        fileIn.close();
+        fileOut.close();
+    }
+}
