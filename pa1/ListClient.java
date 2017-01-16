8638a2495ace8df4c176e57c1e51e13bffc22cd5
spenceralbrecht <s.spencer.a@gmail.com>
2017-01-15 20:36:33 -0800

    Updated ListClient.java for better testing

--- pa1/ListClient.java
+++ pa1/ListClient.java
@@ -16,31 +16,31 @@ public class ListClient{
       System.out.println(A);
       System.out.println(B);
 
-      for(A.moveFront(); A.index()>=0; A.moveNext()){
-         System.out.print(A.get()+" ");
-      }
-      System.out.println();
-      for(B.moveBack(); B.index()>=0; B.movePrev()){
-         System.out.print(B.get()+" ");
-      }
-      System.out.println();
-
-      List C = A.copy();
-      System.out.println(A.equals(B));
-      System.out.println(B.equals(C));
-      System.out.println(C.equals(A));
-
-      A.moveFront();
-      for(int i=0; i<5; i++) A.moveNext(); // at index 5
-      A.insertBefore(-1);                  // at index 6
-      for(int i=0; i<9; i++) A.moveNext(); // at index 15
-      A.insertAfter(-2);
-      for(int i=0; i<5; i++) A.movePrev(); // at index 10
-      A.delete();
-      System.out.println(A);
-      System.out.println(A.length());
-      A.clear();
-      System.out.println(A.length());
+      // for(A.moveFront(); A.index()>=0; A.moveNext()){
+      //    System.out.print(A.get()+" ");
+      // }
+      // System.out.println();
+      // for(B.moveBack(); B.index()>=0; B.movePrev()){
+      //    System.out.print(B.get()+" ");
+      // }
+      // System.out.println();
+      //
+      // List C = A.copy();
+      // System.out.println(A.equals(B));
+      // System.out.println(B.equals(C));
+      // System.out.println(C.equals(A));
+      //
+      // A.moveFront();
+      // for(int i=0; i<5; i++) A.moveNext(); // at index 5
+      // A.insertBefore(-1);                  // at index 6
+      // for(int i=0; i<9; i++) A.moveNext(); // at index 15
+      // A.insertAfter(-2);
+      // for(int i=0; i<5; i++) A.movePrev(); // at index 10
+      // A.delete();
+      // System.out.println(A);
+      // System.out.println(A.length());
+      // A.clear();
+      // System.out.println(A.length());
    }
 }
 
