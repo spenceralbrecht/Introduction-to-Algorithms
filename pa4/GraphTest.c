23974f83e6f927c9cfc5472869d80bb557dcb862
Spencer Albrecht <Spencer@college8-32-236.resnet.ucsc.edu>
2017-02-19 13:27:05 -0800

    Creating neccessary files

---
+++ pa4/GraphTest.c
@@ -0,0 +1,14 @@
+//-----------------------------------------------------------------------------
+// Spencer Albrecht
+// salbrech
+// PA4 CS101
+// GraphTest.c
+// Independently tests Graph interface
+//-----------------------------------------------------------------------------
+#include<stdio.h>
+#include<stlib.h>
+#include "Graph.h"
+int main(int argc, char const *argv[]) {
+    /* code */
+    return 0;
+}
